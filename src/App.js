ef7ac0eb1db1ffb0657cde3830bb4e4a93f0d9f8
malay16 <35947406+malay16@users.noreply.github.com>
2020-03-27 03:16:10 +0530

    Adding Layout in the project

--- src/App.js
+++ src/App.js
@@ -7,40 +7,13 @@ import PasswordReset from './container/PasswordReset';
 import Sidebar from "react-sidebar";
 import classes from './App.css'
 import Poster from './component/Output/Poster'
-
+import Layout from './Layout'
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sidebarOpen: true
-    };
-    this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
-  }
   
-  onSetSidebarOpen(open) {
-    this.setState({ sidebarOpen: open });
-  }
-
   render() {
     return (
       <div >
-        <Sidebar
-        sidebar={
-          <ul>
-          <li className={classes.NavigationItem}><NavLink  to="/"activeClassName={classes.active}>Home</NavLink></li>
-          <li className={classes.NavigationItem}><NavLink  to="/PasswordReset"activeClassName={classes.active}>Password Reset</NavLink></li>
-          <li className={classes.NavigationItem}><NavLink  to="/Poster"activeClassName={classes.active}>Nectar Infotel</NavLink></li>
-        </ul>  
-        
-        }
-        open={this.state.sidebarOpen}
-        onSetOpen={this.onSetSidebarOpen}
-        styles={{ sidebar: { background: "blue" } }}
-      >
-        <button style={{"color":"black","text-decoration": "underline","background-color": "chartreuse"}} onClick={() => this.onSetSidebarOpen(true)}>
-          Important Links
-        </button>
-      </Sidebar>
+        <Layout>
         <Switch>
           <Route path="/" exact component={Login}></Route>
           <Route
@@ -49,6 +22,7 @@ class App extends Component {
           <Route path={"/PasswordReset"} component={PasswordReset}></Route>      
           <Route path={"/Poster"} component={Poster}></Route>      
         </Switch>
+        </Layout>
       </div>
     );
   }
---
+++ src/Layout.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import EmployeePage from './component/Output/EmployeePage'
+import './App.css';
+import { Switch, Route,NavLink } from 'react-router-dom';
+import Login from './container/Login';
+import PasswordReset from './container/PasswordReset';
+import Sidebar from "react-sidebar";
+import classes from './App.css'
+import Poster from './component/Output/Poster'
+
+class Layout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sidebarOpen: true
+    };
+    this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+  }
+  
+  onSetSidebarOpen(open) {
+    this.setState({ sidebarOpen: open });
+  }
+
+  render() {
+    return (
+      <div >
+        <Sidebar
+        sidebar={
+          <ul>
+          <li className={classes.NavigationItem}><NavLink  to="/"activeClassName={classes.active}>Home</NavLink></li>
+          <li className={classes.NavigationItem}><NavLink  to="/PasswordReset"activeClassName={classes.active}>Password Reset</NavLink></li>
+          <li className={classes.NavigationItem}><NavLink  to="/Poster"activeClassName={classes.active}>Nectar Infotel</NavLink></li>
+        </ul>  
+        
+        }
+        open={this.state.sidebarOpen}
+        onSetOpen={this.onSetSidebarOpen}
+        styles={{ sidebar: { background: "blue" } }}
+      >
+        <button style={{"color":"black","text-decoration": "underline","background-color": "chartreuse"}} onClick={() => this.onSetSidebarOpen(true)}>
+          Important Links
+        </button>
+        {this.props.children}
+      </Sidebar>
+      </div>
+    );
+  }
+}
+
+export default Layout;
